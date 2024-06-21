4964b2af624e8a1b4369dbc03845a175feab6327
bekir_filics <bostanci@filics.eu>
2024-06-21 11:18:18 +0200

    feat: create global toaster function

---
+++ src/utils/General.ts
@@ -0,0 +1,10 @@
+import { useToast } from "@/components/ui/toast/use-toast";
+
+const { toast } = useToast();
+
+export function showToast(title: string, description: string) {
+  toast({
+    title: title,
+    description: description,
+  });
+}
