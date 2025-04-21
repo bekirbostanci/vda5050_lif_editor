082db62ff4b8e16744f403ccdeb14add509e15f2
bekirbostanci <bekirbostanci@gmail.com>
2025-04-21 12:10:13 +0200

    feat: add @vercel/analytics package and integrate into App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import LifEditor from './views/LifEditor.vue';
 import Toaster from '@/components/ui/toast/Toaster.vue';
+import { Analytics } from "@vercel/analytics/vue";
 </script>
 
 <template>
+  <Analytics />
   <Toaster />
   <LifEditor></LifEditor>
 </template>
